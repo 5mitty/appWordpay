89cc51aca45def162fe0b0724796f6cc5c8e1063
Jacob Smith <jsmith0361@jh-mba158-19.local>
2018-11-28 11:45:26 -0600

    added graphic user face for button, textView, and textField and added segue

---
+++ appWordpay/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  appWordpay
+//
+//  Created by Jacob Smith on 11/28/18.
+//  Copyright © 2018 John Hersey High School. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    //properties:
+    
+    
+    //functions:
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+}
--- appWordpay/ViewController.swift
+++ appWordpay/ViewController.swift
@@ -9,12 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var placeFirstViewTextField: UITextField!
+    @IBOutlet weak var sentenceTextField: UITextView!
+    
+    //properties:
+    
 
+    //functions:
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let nvc = segue.destination as! ViewController
+    }
 
 }
 
